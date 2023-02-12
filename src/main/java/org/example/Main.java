1c232685d21e9726c74a6568ab6383d41965755e
JavaFaraon <ivan@rybakovka.com>
2023-02-12 21:46:48 +0200

    Finish

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,15 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Main().run();
+    }
+
+    private void run() {
+        int[] nums = new int[]{1,2,3,4,10};
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.runningSum(nums)));
     }
 }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,2 +1,13 @@
-package org.example;public class Solution {
+package org.example;
+
+public class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] runningSums = new int[nums.length];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum+=nums[i];
+            runningSums[i]=sum;
+        }
+        return runningSums;
+    }
 }
